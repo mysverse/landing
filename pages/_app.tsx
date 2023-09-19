027223a397cd08d73fa4ca06030aa6d522a02258
Tan Jun Yan <yan@yan3321.com>
2023-09-20 02:00:54 +0800

    fix: fontawesome icons large size flash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,12 @@
 import "../styles/globals.css";
+
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import type { AppProps } from "next/app";
 // import { useEffect } from "react";
 import PlausibleProvider from "next-plausible";
