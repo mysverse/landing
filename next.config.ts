9ae50a0a37d1f8dee86f49fd9357945a194cf95c
Tan Jun Yan <yan@yan3321.com>
2025-08-07 04:25:42 +0800

    feat: update remote pattern for gravatar in next.config

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "gravatar.com",
+        hostname: "**.gravatar.com",
         pathname: "/avatar/**"
       }
     ]
