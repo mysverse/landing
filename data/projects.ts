a1d64d217192298e92a2b120eaf8ee03df578c9a
Tan Jun Yan <yan@yan3321.com>
2025-12-29 20:42:31 +0800

    fix(projects): remove duplicate ADCS entry

--- data/projects.ts
+++ data/projects.ts
@@ -216,16 +216,6 @@ export const projects: Project[] = [
     link: "/blog/mys/adcs-2025"
   },
   {
-    name: "ADCS 2025",
-    launched: "September 2025",
-    location: "Persada Johor",
-    tagline:
-      "We attended the ASEAN Digital Content Summit 2025, Southeast Asia's premier gaming and digital content event! With our Lebuhraya racing challenges drawing crowds, we connected with hundreds of attendees, while celebrating the region's $6 billion gaming market milestone.",
-    image: OutreachAdcs25,
-    type: "Outreach",
-    link: "/blog/mys/adcs-2025"
-  },
-  {
     name: "SEARCCT",
     launched: "August 2025",
     location: "vOffice Empire Tower",
